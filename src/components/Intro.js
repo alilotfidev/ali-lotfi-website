f1f62e01fe3a353dc377dc1225dbe961ff9db22a
alilotfi.dev <lotfi26.ali@gmail.com>
2020-09-02 10:07:19 +0430

    logo added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import '../assets/css/Intro.css';
-import DownArrow from '../assets/images/arrow.svg';
+import React from "react";
+import "../assets/css/Intro.css";
+import DownArrow from "../assets/images/arrow.svg";
+import Logo from "../assets/images/logo.png";
 
 const Intro = ({ scrollTo }) => {
-    return(
-        <div className="intro">
-            <div className="intro-texts">
-                <h2>I'm <span>Ali</span></h2>
-                <p className="details">A <strong>Hard-working</strong> Front-End Developer based in Iran.</p>
-                <button className="btn-primary" onClick={() => {scrollTo('AboutMe')}}>About Me</button>
-            </div>
-            <div id="intro-logo" className="logo">Ali</div>
-            <img className="down-arrow" src={DownArrow} alt="go down" onClick={() => {scrollTo('AboutMe')}}/>
-        </div>
-    )
-}
+  return (
+    <div className="intro">
+      <div className="intro-texts">
+        <h2>
+          I'm <span>Ali</span>
+        </h2>
+        <p className="details">
+          A <strong>Hard-working</strong> Front-End Developer based in Iran.
+        </p>
+        <button
+          className="btn-primary"
+          onClick={() => {
+            scrollTo("AboutMe");
+          }}
+        >
+          About Me
+        </button>
+      </div>
+      <img className="big-logo-image" src={Logo} alt="Ali Lotfi" />
+      <img
+        className="down-arrow"
+        src={DownArrow}
+        alt="go down"
+        onClick={() => {
+          scrollTo("AboutMe");
+        }}
+      />
+    </div>
+  );
+};
 
 export default Intro;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../assets/css/Navbar.css';
+import Logo from "../assets/images/logo.png";
 
 const Navbar = ({ scrollTo }) => {
   return (
     <div className="Navbar">
       <nav>
-        <div className="logo" onClick={() => {scrollTo('Intro')}}>Ali Lotfi<br/><span>Web Developer</span></div>
+        <div className="logo" onClick={() => {scrollTo('Intro')}}><img src={Logo} alt="ali lotfi"/><div> Ali Lotfi<br/><span>Web Developer</span></div></div>
         <ul className="links">
           <li className="navLink" onClick={() => {scrollTo('AboutMe')}}>About Me</li>
           <li className="navLink" onClick={() => {scrollTo('Portfolio')}}>Portfolio</li>
